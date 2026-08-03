345258254e8fa9dd9ffe471d9a386d1a847863c9
Amber Wall <jamber0513@icloud.com>
2026-08-02 22:22:46 -0500

    cap fix

--- script.js
+++ script.js
@@ -287,7 +287,7 @@ campfire: {
           changeEnergy(20);
           alert("You drink a wonderfully, warming coffee. +20 Energy!")
         },
-        next: "Campfire"
+        next: "campfire"
       },
       {
         text: "Return to Forest🌳",
